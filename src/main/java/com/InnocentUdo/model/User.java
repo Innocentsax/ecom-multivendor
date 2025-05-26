b7075ecc54954c1b19edccdbf5c579cc73e313e0
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-05-26 20:08:47 +0800

    Entity relational mapping

--- src/main/java/com/InnocentUdo/model/User.java
+++ src/main/java/com/InnocentUdo/model/User.java
@@ -60,13 +60,5 @@ public class User {
     public void setEmail(String email) {
         this.email = email;
     }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
     
 }
